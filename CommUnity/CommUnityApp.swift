935dd8e0fb5a96f6915d86f1d46ed8f8ee89a33f
Jake Pacia <christienjakedavidpacia@gmail.com>
2026-05-05 10:28:26 +0800

    Initial Commit

---
+++ CommUnity/CommUnityApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommUnityApp.swift
+//  CommUnity
+//
+//  Created by Christien Jake Pacia on 5/5/26.
+//
+
+import SwiftUI
+
+@main
+struct CommUnityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
